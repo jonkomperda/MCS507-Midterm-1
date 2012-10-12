19e76e24401dd51e0997963aab3c572a9b2e7e56
Jon Komperda <komperda.jon@gmail.com>
2012-10-12 12:58:59 -0500

    implemented the dictionary in problem 2

--- src/problem2.py
+++ src/problem2.py
@@ -1,21 +1,26 @@
 class stirling():
     """docstring for stirling"""
     def __init__(self, n, k):
-        self.n = n
-        self.k = k
-        self.dict = {(0,0):1}
+        self.dict = {(0,0):1} #start with the initial condition (saves an if statement)
+        self.answer = self.c(n,k)
         
     
     def c(self, n, k):
         """docstring for compute"""
-        if (n==0) and (k==0):
-            return 1
-        if (n <= 0) or (k <= 0):
+        if (n,k) in self.dict.keys():
+            #print 'using dict for: '+str((n,k)) #checks to see if we used the dictionary or not
+            return self.dict[(n,k)]
+        elif n<=0 or k<=0:
             return 0
         else:
             ans = - (n-1)*self.c(n-1,k) + self.c(n-1,k-1)
+            self.dict[(n,k)] = ans
             return ans
+    
+    def __str__(self):
+        """string representation of our data"""
+        return 'Computed answer: '+str(self.answer)+'\nDictionary Values: \n'+str(self.dict)
 
 if __name__ == '__main__':
-    a = stirling(3,2)
-    print a.c(5,5)
+    a = stirling(5,2)
+    print a
